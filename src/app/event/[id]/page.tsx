2b9a085bb64a217bc79cd99d970116b96f247337
ntzolov <ntzolov@gmail.com>
2024-05-29 22:10:27 +0300

    create page for event info

---
+++ src/app/event/[id]/page.tsx
@@ -0,0 +1,10 @@
+import BackButton from './BackButton';
+
+export default function EventPage() {
+  return (
+    <div className='flex flex-col items-center text-gray-300 text-lg'>
+      <h1>Event</h1>
+      <BackButton />
+    </div>
+  );
+}
