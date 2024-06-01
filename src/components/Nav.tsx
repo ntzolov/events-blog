44315622b0233a01b9b4c85c0df2c34d770e79e3
ntzolov <ntzolov@gmail.com>
2024-06-01 16:48:33 +0300

    update nav links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,40 @@
+'use client';
+
 import Link from 'next/link';
-import NavButton from './NavButton';
+import { usePathname } from 'next/navigation';
 
 export default function Nav() {
+  const path = usePathname();
   return (
     <nav className='w-full flex flex-col items-center my-5 gap-3 sm:flex-row justify-center'>
-      <Link href={'/'}>
-        <NavButton>Home</NavButton>
+      <Link
+        className={`${
+          path === '/' ? 'bg-lime-600' : 'bg-lime-700'
+        } inline-flex items-center justify-center rounded-md px-4 py-2 text-md font-medium text-gray-200 shadow transition-colors hover:bg-lime-600 focus:outline-none active:scale-90`}
+        href={'/'}>
+        Home
+        {/* <NavButton>Home</NavButton> */}
       </Link>
-      <Link href={'/create'}>
-        <NavButton>Create event</NavButton>
+      <Link
+        className={`${
+          path === '/create' ? 'bg-lime-600' : 'bg-lime-700'
+        } inline-flex items-center justify-center rounded-md px-4 py-2 text-md font-medium text-gray-200 shadow transition-colors hover:bg-lime-600 focus:outline-none active:scale-90`}
+        href={'/create'}>
+        Create event
       </Link>
-      <Link href={'/my-events'}>
-        <NavButton>My events</NavButton>
+      <Link
+        className={`${
+          path === '/my-events' ? 'bg-lime-600' : 'bg-lime-700'
+        } inline-flex items-center justify-center rounded-md px-4 py-2 text-md font-medium text-gray-200 shadow transition-colors hover:bg-lime-600 focus:outline-none active:scale-90`}
+        href={'/my-events'}>
+        My events
       </Link>
-      <Link href={'/all-events'}>
-        <NavButton>All events</NavButton>
+      <Link
+        className={`${
+          path === '/all-events' ? 'bg-lime-600' : 'bg-lime-700'
+        } inline-flex items-center justify-center rounded-md  px-4 py-2 text-md font-medium text-gray-200 shadow transition-colors hover:bg-lime-600 focus:outline-none active:scale-90`}
+        href={'/all-events'}>
+        All events
       </Link>
     </nav>
   );
--- src/components/NavButton.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-export default function NavButton({ children }: { children: ReactNode }) {
-  return (
-    <button className='inline-flex items-center justify-center rounded-md bg-lime-700 px-4 py-2 text-md font-medium text-gray-200 shadow transition-colors hover:bg-lime-600 focus:outline-none active:scale-90'>
-      {children}
-    </button>
-  );
-}
